b2840f09590e2e19b5d816752ee6567b37aabc73
bl1nkker <nurovich14@gmail.com>
2023-10-24 14:55:31 +0500

    feat: variables and datatypes

--- main.go
+++ main.go
@@ -4,6 +4,13 @@ import "fmt"
 
 // Entry point for the application
 func main() {
-	fmt.Println("Hello World")
-	fmt.Print("Hello World")
+	// Declaring a variable
+	var conferenceName string = "Hello Golang Conference"
+	var conferenceTickets int = 50
+	var remainingTickets int = 10
+	fmt.Println("Welcome to", conferenceName)
+	fmt.Printf("We have total %v tickets\n", conferenceTickets)
+	fmt.Printf("We have %d tickets remaining\n", remainingTickets)
+
+	
 }
