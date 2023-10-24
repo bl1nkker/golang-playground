365f45d52a136182aefe23a347933d1cf3449e1e
bl1nkker <nurovich14@gmail.com>
2023-10-24 15:10:26 +0500

    feat: pointers and user input

--- main.go
+++ main.go
@@ -8,9 +8,23 @@ func main() {
 	var conferenceName string = "Hello Golang Conference"
 	var conferenceTickets int = 50
 	var remainingTickets int = 10
+	// Types
+	fmt.Printf("conferenceName is %T, and conferenceTickets is %T, and remainingTickets is %T \n", conferenceName, conferenceTickets, remainingTickets)
 	fmt.Println("Welcome to", conferenceName)
 	fmt.Printf("We have total %v tickets\n", conferenceTickets)
 	fmt.Printf("We have %d tickets remaining\n", remainingTickets)
 
-	
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets int
+	fmt.Print("Enter your first name: ")
+	fmt.Scan(&firstName)
+	fmt.Print("Enter your last name: ")
+	fmt.Scan(&lastName)
+	fmt.Print("Enter your email: ")
+	fmt.Scan(&email)
+	fmt.Print("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+	fmt.Printf("Thank You %v %v for booking %v tickets. You will receive a confirmation email at %v with more details!", firstName, lastName, userTickets, email)
 }
